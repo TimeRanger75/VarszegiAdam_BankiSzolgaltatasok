57f97055314e56f1dbe309eace478964e95aa7b6
Time Ranger <varszegi.adam75@gmail.com>
2022-10-24 14:34:17 +0200

    További tesztek

--- src/hu/petrik/bankiszolgatatasok/Main.java
+++ src/hu/petrik/bankiszolgatatasok/Main.java
@@ -8,9 +8,15 @@ public class Main {
 
         Bank bank=new Bank();
         bank.szamlaNyitas(t1, 1000).befizet(3490);
-        bank.szamlaNyitas(t2,5000).kivesz(3490);
+        //bank.szamlaNyitas(t1,-8).kivesz(2000);
+        bank.szamlaNyitas(t2,5000).befizet(59000);
+        Szamla s_t1=new HitelSzamla(t1, 200);
+        s_t1.befizet(5000);
+        s_t1.kivesz(2000);
+        System.out.println(s_t1.aktualisEgyenleg);
         System.out.println(bank.getOsszHitelkeret());
         System.out.println(bank.getOsszEgyenleg(t2));
+        System.out.println(bank.getLegnagyobbEgyenleguSzamla(t2));
 
     }
 }
